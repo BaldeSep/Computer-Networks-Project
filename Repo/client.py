9ef153a1e698e972c28383a8f38bb1b570c38407
Baldemar Sepulveda <balde.sepu@gmail.com>
2018-03-03 22:56:38 -0800

    A Mostly Finished Client Server Project Will Post Instructions On how To User it soon.

--- Repo/client.py
+++ Repo/client.py
@@ -1,7 +1,11 @@
-import socket, encoding
+import socket
+import base64
+from encoding_package.encoding_mod import encoder
+
+
 
 def validation(conn):
-	packet_size = 1024
+	packet_size = 2048
 	
 	message_type = {'Invalid Credentials': bytes([1]), 
 					'Valid Credentials': bytes([2]),
@@ -30,31 +34,119 @@ def validation(conn):
 			
 	if signed_in == True:
 		print ('You are signed in!!!')
-		command_input(conn, username)
+		session(conn, username)
 	elif rejected == True:
 		print ('You ran out of attempts')
 		conn.close()
 	
-
-
-def command_input(conn, user_name):
-	message_type = {'ascii-yes': bytes([1]),
-					'ascii-no': bytes([2]),}
-	packet_size = 1024
+def send_server_file(conn, ascii_armor, message_type, packet_size):
+	src_file = open('src/file', 'rb')
+	key_file = open('src/key', 'rb')
 	
-	ascii_amoring = conn.recv(packet_size)
-	if ascii_amoring == message_type['ascii-yes']:
-		print ('Yes to ascii amroing!!!')
-	elif ascii_amoring == message_type['ascii-no']:
-		print ('No to ascii amoring!!!')
-
-	conn.close()
+	# Read Some data from the source file
+	data = src_file.read(packet_size//2)
 	
+	# Creates an encoder object to handle encoders
+	e = encoder()
+	
+	response = conn.recv(1)
+	if response == message_type['size-?']:
+		conn.send( (len(data)).to_bytes(2, 'big') )
+	conn.recv(1)
+	
+	# Begin sending data to the server
+	while len(data) > 0:	
+		# Hash the data
+		data = e.hash_(data)
 		
+		# Get a key from the key file
+		key = key_file.read(packet_size)
+		while len(key) < packet_size:
+			key_file.seek(0,0)
+			key = key_file.read(packet_size)
+	
+		# Encrypt the data using XOR method
+		data = e.xor_c(data, key)
+		
+		
+		# If Ascii Armoring is requested apply it to the data chunk
+		if ascii_armor == True:
+			data = e.ascii_armor(data, 'e')
+		
+		# Send size of data after base 64 MIME encoding and adding hash to data
+		response = conn.recv(1)
+		if response == message_type['size-?']:
+			conn.send( (len(data)).to_bytes(2, 'big') )
+			conn.recv(1)
+		
+		# Send the data
+		try:
+			if conn.sendall(data) == None:
+				print('Successfully Sent Data')
+		except:
+			print('Error could not send data')
+
+		# Check to see if the data sent was properly recieved
+		response = conn.recv(1)
+		while response == message_type['bad data']:
+			# send the data again
+			try:
+				if conn.sendall(data) == None:
+					print('Successfully Sent Data')
+			except:
+				print('Error could not send data')
+			
+			print('BAD DATA')
+			
+			# Check again if the data was good
+			response = conn.recv(1)
+		
+		# If too many errors happened end connection
+		if response == message_type['err end comm']:
+			break	
+		
+		# Gather the next chunk of data
+		data = src_file.read(packet_size//2)
+		
+		if response == message_type['size-?']:
+			print('Sending Size')
+			conn.send( (len(data)).to_bytes(2, 'big') )
+			print('Done Sending Size')
+		
+		print('DONE')	
+		conn.recv(1)
+	else:
+		# Once done send a done message
+		conn.send(message_type['done'])
+
+	
+	
+	# Closes files
+	src_file.close()
+	key_file.close()
+	
+def session(conn, user_name):
+	packet_size = 2048
+	
+	message_type = {'ascii-yes': bytes([1]),
+					'ascii-no': bytes([2]),
+					'ascii-?': bytes([3]),
+					'bad data': bytes([4]),
+					'err end comm': bytes([5]),
+					'done': bytes([6]),
+					'size-?': bytes([7]),}
+	
+	ascii_armor = False
+	conn.send(message_type['ascii-?'])
+	if conn.recv(packet_size) == message_type['ascii-yes']:
+		ascii_armor = True
+	
+	send_server_file(conn, ascii_armor, message_type, packet_size)
+	conn.close()	
 		
 
 s = socket.socket()
-host = '127.0.0.1'
+host = '192.168.0.107'
 port = int(input('Input a port number: '))
 
 
--- Repo/server.py
+++ Repo/server.py
@@ -1,4 +1,5 @@
-import socket, encoding
+import socket
+from encoding_package.encoding_mod import encoder
 
 def load_data_base(data_base):
 	# Reads in all user data to be verified of the user
@@ -22,8 +23,11 @@ def validate(conn):
 					'Reject': bytes([3])} 
 					
 	while attempts_left > 0:
-		username = conn.recv(packet_size).decode('utf-8')
-		password = conn.recv(packet_size).decode('utf-8')
+		username = conn.recv(packet_size)
+		username = username.decode('utf-8')
+		password = conn.recv(packet_size)
+		password = password.decode('utf-8')
+		
 		
 		try:
 			if data_base[username] == password:
@@ -47,26 +51,135 @@ def validate(conn):
 	else:
 		conn.close()
 
+
+
+
+def server_wants_ascii_armoring():
+	response = input('Do you want to apply ascii armoring? (y/n)?: ')
+	if response == 'y':
+		return True
+	
+	return False
+
+def recieve_file_data(conn, ascii_armor, message_type, packet_size):
+	
+	dest_file = open('dest/file', 'wb')
+	key_file = open('dest/key', 'rb')
+	
+	conn.send(message_type['size-?'])
+	data_size = int.from_bytes(conn.recv(packet_size), 'big')
+	conn.send(message_type['done'])
+	
+	conn.send(message_type['size-?'])
+	total_size = int.from_bytes(conn.recv(packet_size), 'big')
+	conn.send(message_type['done'])
+	
+	# Recieve data from client
+	data = conn.recv(packet_size)
+	while len(data) < (total_size):
+		data = data + conn.recv(packet_size)
+	
+	# While client is not done sending data keep
+	# processing incomming data
+	while data != message_type['done'] and len(data) > 1:
+		
+		# Get key from key file
+		key = key_file.read(packet_size)
+		while len(key) < packet_size:
+			key_file.seek(0,0)
+			key = key_file.read(packet_size)
+		
+		# Get hash from processing function
+		hash_, data = process_data(data, key, ascii_armor, data_size)
+		
+		
+		attempts_left = 10
+		# Check for integrity
+		while hash_ != data[data_size:] and attempts_left > 0:
+			attempts_left -= 1
+			print('bad data')
+			conn.send(message_type['bad data'])
+			
+			data = conn.recv(packet_size)
+			while len(data) < total_size:
+				data = data + conn.recv(packet_size)
+			
+			hash_, data = process_data(data, key, ascii_armor, data_size)
+		
+		data = data[:data_size]
+		
+		dest_file.write(data)
+		
+		# request to size of data before Base64 MIME encoding and added hash		
+		conn.send(message_type['size-?'])
+		data_size = int.from_bytes(conn.recv(packet_size), 'big')
+		conn.send(message_type['done'])
+		
+		# request for size of data actually sent with added hash and Base64 Mime Encoding
+		conn.send(message_type['size-?'])
+		total_size = int.from_bytes(conn.recv(packet_size), 'big')
+		conn.send(message_type['done'])
+		
+		print("Successful Transfer Of Data")
+		
+		# Recieve the next chunk of data
+		print('Getting Next Chunk of Data from Client')
+		data = conn.recv(packet_size)
+		while len(data) < total_size:
+			data = data + conn.recv(packet_size)
+	
+	conn.send(message_type['done'])
+	
+	dest_file.close()
+	key_file.close()
+
+def process_data(data, key, ascii_armor, data_size):
+	e = encoder()
+	# decode base 64 mime encoding if applied
+	if ascii_armor == True:
+		data = e.ascii_armor(data, 'd')
+		
+	# Decrypt data
+	data = e.xor_c(data, key)
+	
+	# Get the hash from the data
+	hash_ = data[data_size:]
+	
+	# Remove hash from data
+	data = data[:data_size]
+
+	print(len(data))
+
+	# Recompute hash for comparison
+	data = e.hash_(data)
+	
+	return hash_, data
+	
 def session(conn):
+	packet_size = 2048	
 	message_type = {'ascii-yes': bytes([1]),
-					'ascii-no': bytes([2]),}
+					'ascii-no': bytes([2]),
+					'ascii-?': bytes([3]),
+					'bad data': bytes([4]),
+					'err end comm': bytes([5]),
+					'done': bytes([6]),
+					'size-?': bytes([7]),}
 	
-	packet_size = 1024
+	ascii_armor = False
+	if conn.recv(packet_size) == message_type['ascii-?']:
+		if server_wants_ascii_armoring() == True:
+			conn.send(message_type['ascii-yes'])
+			ascii_armor = True
+		else:
+			conn.send(message_type['ascii-no'])
 	
-	ascii_amoring = input('Do you want ascii amoring?(y/n)?: ')
-	if ascii_amoring == 'y':
-		conn.send(message_type['ascii-yes'])
-	else:
-		conn.send(message_type['ascii-no'])
-		
+	recieve_file_data(conn, ascii_armor, message_type, packet_size)
+	
 	conn.close()
-		
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # local host
-host = '127.0.0.1'
+host = '192.168.0.107'
 port = int(input('Input a port number: '))
 s.bind((host, port))
 
